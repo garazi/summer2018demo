cb62b37a8482413b855dc5fb36fdf1aeea8a1759
garazi <grewis@grewis-ltm4.internal.salesforce.com>
2018-05-15 15:29:44 -0700

    updated show and hide

--- force-app/main/default/aura/customLayout/customLayoutController.js
+++ force-app/main/default/aura/customLayout/customLayoutController.js
@@ -4,7 +4,7 @@
         $A.util.toggleClass(f, "slds-hide");
         var m = component.find("mainCol");
         if (m.get("v.size") == "12") {
-            m.set("v.size", "9");
+            m.set("v.size", "8");
         } else {
             m.set("v.size", "12");
         }
