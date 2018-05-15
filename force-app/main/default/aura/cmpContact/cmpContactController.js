0a74a1235ba641301d7c67ad32490517e7a666ef
garazi <grewis@grewis-ltm4.internal.salesforce.com>
2018-05-15 09:46:02 -0700

    initial commit

---
+++ force-app/main/default/aura/cmpContact/cmpContactController.js
@@ -0,0 +1,28 @@
+({
+    doInit : function(component,event,helper){
+        var declarativeFields=component.get("v.fieldsToDisplay");
+        if (declarativeFields) {
+            var inputSplit=declarativeFields.split(",");
+            for (var i in inputSplit) {
+                inputSplit[i]=inputSplit[i].trim();
+            }
+            component.set("v.myFields", inputSplit);
+        }
+    },
+    happyDance : function(component, event, helper) {
+        var evt = $A.get("e.c:recordChange");
+        evt.setParams({
+            "message" : component.get('v.recordId') });
+        evt.fire();
+    },
+    overrideSave : function(component,event,helper) {
+        console.log("override")
+        event.preventDefault();
+    },
+    prefillForm : function(component,event,helper){
+        var tmpId = event.getParam("message");
+        console.log("here: ", tmpId)
+        var form = component.find('recordViewForm');
+        form.set('v.recordId', tmpId);
+    }
+})
